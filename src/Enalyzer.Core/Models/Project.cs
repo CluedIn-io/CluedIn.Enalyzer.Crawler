1556f57ff9b9c6e3e6aa63e4b3caced05efe13a2
Ziggy <zygis0408@gmail.com>
2020-07-28 09:45:35 +0200

    added model

---
+++ src/Enalyzer.Core/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CluedIn.Crawling.Enalyzer.Core.Models
+{
+    public class Projects
+    {
+        public List<Project> projects { get; set; }
+    }
+    public class Project
+    {
+        public string id { get; set; }
+    }
+}
